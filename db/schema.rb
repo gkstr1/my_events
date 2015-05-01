111fabcce9b42c6711bc6483e52b42830be5b07f
Glen Kutsovsky <gkstr1@hotmail.com>
2015-05-01 15:50:10 -0400

    added friendly_id slugs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430151424) do
+ActiveRecord::Schema.define(version: 20150501193426) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150430151424) do
     t.integer  "organizer_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "slug"
   end
 
   create_table "users", force: :cascade do |t|
